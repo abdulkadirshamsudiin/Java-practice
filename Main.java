4d996bcd961672afb736bd29f278ede636f261e8
Shamsudin Abdulkadir Shariff <abdulkadirshamsu09@gmail.com>
2025-03-24 07:04:46 +0000

    Added Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main{ 
+    // Public  funcition - Is an access modifier. allow all the code to be accessible
+    // void returns nothing. the name of the fucition is
+    public static void main(String[] args){
+        
+        String name ="alex";
+        int Age = 17;
+        byte mynum = Byte.MAX_VALUE;
+
+        System.out.println("hello, My name is " + name);
+        System.out.println("I am " + Age +" years old" );
+        System.out.println(mynum);
+        System.out.println(mynum);
+
+
+    }
+}
